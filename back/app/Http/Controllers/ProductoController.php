a3d69bd4e2b8d1d2c91073d6a8ca18bd4be64f21
adimer <adimer101@gmail.com>
2025-02-25 17:07:03 -0400

    Actulizado para la creacion de productos

--- back/app/Http/Controllers/ProductoController.php
+++ back/app/Http/Controllers/ProductoController.php
@@ -7,13 +7,26 @@ use Illuminate\Http\Request;
 
 class ProductoController extends Controller{
     function index(Request $request){
-        $paginate = 50;
+        $paginate = 30;
         $filter = $request->input('filter');
+        $page = $request->input('page');
 
         $productos = Producto::where('medicamento', 'like', "%$filter%")
             ->orWhere('forma_farmaceutica', 'like', "%$filter%")
             ->orWhere('concentracion', 'like', "%$filter%")
+            ->orderBy('medicamento')
             ->paginate($paginate);
         return response()->json($productos);
+    }
+    function store(Request $request){
+        return Producto::create($request->all());
+    }
+    function update(Request $request, Producto $producto){
+        $producto->update($request->all());
+        return $producto;
+    }
+    function destroy(Producto $producto){
+        $producto->delete();
+        return response()->json(['success' => true]);
     }
 }
